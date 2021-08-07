dead3c60755119a8a890b068f161eb7aa43ea4be
awesomemusician <chris@awesomemusician.com>
2021-08-07 19:15:27 +0200

    add output

--- configure-ubuntu.sh
+++ configure-ubuntu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Running docker install script"
 curl https://get.docker.com/ | sudo sh
 
+echo "Install docker config"
 sudo cp etc/docker/daemon.json /etc/docker
 
+echo "Allow user to run docker commands"
 sudo usermod -aG docker user
 
 sudo apt install -y unattended-upgrades python3-pip tree curl net-tools bridge-utils tshark tmux dnsutils screen htop git make g++ ncdu iftop unzip
